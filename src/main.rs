b120a672f592614dafc6ad508692d6e9c33e2103
bear <boundless.forest@outlook.com>
2022-12-19 10:29:02 +0800

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // 1. Add https://github.com/ethereum-lists/chains as git-submodule
+    println!("Hello, world!");
+    // 2. Add cli command
+}
+
+// eth-chains-cli list
+// eth-chains-cli add
+// eth-chains-cli find-chain id
