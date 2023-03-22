dcf7613fa12152699af7d5b956e8a67eecfefecb
bear <boundless.forest@outlook.com>
2023-03-22 20:58:09 +0800

    Auto download

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,38 @@ mod types;
 use anyhow::{Context, Result};
 use clap::Parser;
 use cli::{Action, Cli};
+use prettytable::{Cell, Row, Table};
 use std::fs::File;
+use std::path::Path;
 use types::ChainInfo;
 use walkdir::WalkDir;
-use prettytable::{Cell, Row, Table};
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
+
+    let remote_url = "https://github.com/ethereum-lists/chains.git";
+    let local_path = Path::new("chains");
+
+    let mut proxy_opts = git2::ProxyOptions::new();
+    proxy_opts.url("http://127.0.0.1:7890"); // remove this line if you don't use proxy
+
+    if let Ok(repo) = git2::Repository::open(local_path) {
+        println!("bear: --- into the fetch branch");
+        let mut fo = git2::FetchOptions::new();
+        fo.proxy_options(proxy_opts);
+
+        let mut remote = repo.find_remote("origin")?;
+        remote.fetch(&["master"], Some(&mut fo), None)?;
+    } else {
+        println!("bear: --- into the clone branch");
+        let mut fo = git2::FetchOptions::new();
+        fo.proxy_options(proxy_opts);
+
+        let mut builder = git2::build::RepoBuilder::new();
+        builder.fetch_options(fo);
+
+        builder.clone(remote_url, local_path)?;
+    }
 
     match &cli.action {
         Action::List => {
@@ -44,7 +69,7 @@ fn main() -> Result<()> {
             ))
             .with_context(|| format!("NO chain associated with this id now"))?;
             let chain_info: ChainInfo = serde_json::from_reader(file).unwrap();
-            
+
             println!("{}", serde_json::to_string(&chain_info).unwrap());
         }
         Action::ByName { name } => {
